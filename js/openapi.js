063c348f43e9ddc4a9442f955a8e5193cd70dfc7
Jessica Sosa <jessicasosa444@gmail.com>
2026-06-17 07:28:54 -0400

    finished weather app

--- js/openapi.js
+++ js/openapi.js
@@ -4,12 +4,14 @@ const currentButton = document.querySelector("#current-weather");
 const forecastButton = document.querySelector("#forecast-weather");
 
 currentButton.addEventListener("click", function () {
-  fetch("https://api.open-meteo.com/v1/forecast?latitude=40.74&longitude=-74.03&current=temperature_2m,wind_speed_10m&temperature_unit=fahrenheit&wind_speed_unit=mph")
+  fetch(
+    "https://api.open-meteo.com/v1/forecast?latitude=40.74&longitude=-74.03&current=temperature_2m,wind_speed_10m&temperature_unit=fahrenheit&wind_speed_unit=mph",
+  )
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-    results.innerHTML = `
+      results.innerHTML = `
     <h2>🌤️ Current Weather</h2>
     <p>🌡️ Temperature: ${data.current.temperature_2m}°F</p>
     <p>💨 Wind Speed: ${data.current.wind_speed_10m} mph</p>
@@ -22,7 +24,9 @@ currentButton.addEventListener("click", function () {
 });
 
 forecastButton.addEventListener("click", function () {
-  fetch("https://api.open-meteo.com/v1/forecast?latitude=40.74&longitude=-74.03&daily=temperature_2m_max,temperature_2m_min&forecast_days=1&temperature_unit=fahrenheit")
+  fetch(
+    "https://api.open-meteo.com/v1/forecast?latitude=40.74&longitude=-74.03&daily=temperature_2m_max,temperature_2m_min&forecast_days=1&temperature_unit=fahrenheit",
+  )
     .then(function (response) {
       return response.json();
     })
